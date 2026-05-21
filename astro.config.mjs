53ad36d6c0ca09d084dc7554538de5bd8b174da2
gamerecharger <clarence.chia@gamerecharger.com>
2026-05-21 12:00:06 +0800

    Update site URL for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://time-internet-promo.pages.dev',
+  site: 'https://timeinternetpromo.my',
   integrations: [sitemap()],
 });
